d6cf006f1846c37808823e242ff1ada528743f14
Oh Byeongkuk <lunamundus1007@gmail.com>
2023-07-21 01:41:55 +0900

    study 230720

---
+++ stream.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+Future<int> sumStream(Stream<int> stream) async {
+  var sum = 0;
+  await for (var value in stream) {
+    print('sumStream : $value');
+    sum += value;
+  }
+
+  return sum;
+}
+
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= 10; i++) {
+    print('countStream : $i');
+    yield i;
+  }
+}
+
+main() async {
+  var stream = countStream(10);
+  var sum = await sumStream(stream);
+  print(sum);                                         // 55
+}
