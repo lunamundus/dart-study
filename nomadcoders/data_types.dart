e9cd42e1b1fb703b7cff581fd86fa0e90bc634d5
Oh Byeongkuk <lunamundus1007@gmail.com>
2023-09-04 03:02:29 +0900

    study 230903

--- nomadcoders/data_types.dart
+++ nomadcoders/data_types.dart
@@ -1,1 +1,96 @@
+// #2.0 Basic Data Types
+// void main() {
+//   String name = 'luna';
+//   bool alive = true;
+//   int age = 12;
+//   double money = 69.99;
 
+//   num x = 12;
+//   x = 1.2;
+// }
+
+// #2.1 Lists
+// void main() {
+//   var giveMeFive = true;
+//   var numbers = [
+//     1,
+//     2,
+//     3,
+//     4,
+//     if (giveMeFive) 5,
+//   ];
+//   print(numbers);
+// }
+
+// void main() {
+//   var giveMeFive = true;
+//   var numbers = [
+//     1,
+//     2,
+//     3,
+//     4,
+//   ];
+//   if (giveMeFive) {
+//     numbers.add(5);
+//   }
+//   print(numbers);
+// }
+
+// #2.2 String Interpolation
+// void main() {
+//   var name = 'luna';
+//   var greeting = 'Hello everyone, my name is $name, nice to meet you!';
+
+//   print(greeting);
+// }
+
+// void main() {
+//   var name = 'luna';
+//   var age = 12;
+//   var greeting = "Hello everyone, my name is $name, I'm ${age + 10} years old.";
+
+//   print(greeting);
+// }
+
+// #2.3 Collection For
+// void main() {
+//   var oldFriends = ['Lucas', 'Lynn'];
+//   var newFriends = [
+//     'Lewis',
+//     'Ralph',
+//     'Darren',
+//     for (var friend in oldFriends) "$friend",
+//   ];
+
+//   print(newFriends);
+// }
+
+// #2.4 Maps
+// void main() {
+//   var player = {
+//     'name': 'luna',
+//     'xp': 19.99,
+//     'superpower': false,
+//   };
+// }
+
+// void main() {
+//   Map<int, bool> player = {
+//     1: true,
+//     2: false,
+//     3: true,
+//   };
+// }
+
+void main() {
+  List<Map<String, Object>> players = [
+    {
+      'luna': true,
+      'xp': 1.35,
+    },
+    {
+      'lucas': false,
+      'xp': 2.67,
+    },
+  ];
+}
