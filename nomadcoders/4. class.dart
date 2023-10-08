76afff63cc2ce22296ad1d6433efacc581abb051
Oh Byeongkuk <lunamundus1007@gmail.com>
2023-10-08 21:30:13 +0900

    study: 231008 다트 언어의 Class 학습

--- nomadcoders/4. class.dart
+++ nomadcoders/4. class.dart
@@ -188,10 +188,12 @@
 // #4.6 Enums
 // enum Team { red, blue }
 
+// enum XPLevel { beginner, medium, pro }
+
 // class Player {
 //   String name;
-//   int xp;
-//   String team;
+//   XPLevel xp;
+//   Team team;
 
 //   Player({
 //     required this.name,
@@ -205,10 +207,153 @@
 // }
 
 // void main() {
-//   var luna = Player(name: 'Luna', xp: 1200, team: 'Blue') // cascade notation
-//     ..name = 'luna'
-//     ..xp = 12000
-//     ..team = 'Red';
+//   var luna = Player(
+//     name: 'luna',
+//     xp: XPLevel.medium,
+//     team: Team.blue,
+//   );
 
-//   var exam = Player(name: 'exam', xp: 1000, team: Team.blue);
+//   var potato = luna
+//     ..name = 'lucas'
+//     ..xp = XPLevel.pro
+//     ..team = Team.red
+//     ..sayHello();
+// }
+
+// #4.7 Abstract Classes
+// abstract class Human {
+//   void walk();
+// }
+
+// enum Team { red, blue }
+
+// enum XPLevel { beginner, medium, pro }
+
+// class Player extends Human {
+//   String name;
+//   XPLevel xp;
+//   Team team;
+
+//   Player({
+//     required this.name,
+//     required this.xp,
+//     required this.team,
+//   });
+
+//   void walk() {
+//     print("I'm walking");
+//   }
+
+//   void sayHello() {
+//     print("Hi, my name is $name");
+//   }
+// }
+
+// class coach extends Human {
+//   void walk() {
+//     print("The coach is walking");
+//   }
+// }
+
+// void main() {
+//   var luna = Player(
+//     name: 'luna',
+//     xp: XPLevel.medium,
+//     team: Team.blue,
+//   );
+
+//   var potato = luna
+//     ..name = 'lucas'
+//     ..xp = XPLevel.pro
+//     ..team = Team.red
+//     ..sayHello();
+// }
+
+// #4.8 Inheritance
+// class Human {
+//   final String name;
+
+//   Human({required this.name});
+
+//   void sayHello() {
+//     print('Hi, my name is $name');
+//   }
+// }
+
+// enum Team { red, blue }
+
+// class Player extends Human {
+//   final Team team;
+
+//   Player({
+//     required this.team,
+//     required String name,
+//   }) : super(name: name);
+
+//   @override
+//   void sayHello() {
+//     super.sayHello();
+//     print('and I play for ${team.toString().split('.').last}');
+//     // print('and I play for ${team.toString().split('.').last}');
+//   }
+// }
+
+// void main() {
+//   var player = Player(
+//     team: Team.red,
+//     name: 'luna',
+//   );
+
+//   player.sayHello();
+// }
+
+// #4.9 Mixins
+// class Strong {
+//   final double strengthLevel = 1500.99;
+// }
+
+// class QuickRunner {
+//   void runQuick() {
+//     print("ruuuuuun!!!");
+//   }
+// }
+
+// class Tall {
+//   final double height = 1.99;
+// }
+
+// mixin class Strong {
+//   final double strengthLevel = 1500.99;
+// }
+
+// mixin class QuickRunner {
+//   void runQuick() {
+//     print("ruuuuuun!!!");
+//   }
+// }
+
+// mixin Tall {
+//   final double height = 1.99;
+// }
+
+// enum Team { red, blue }
+
+// class Player with QuickRunner, Strong, Tall {
+//   final Team team;
+
+//   Player({
+//     required this.team,
+//   });
+// }
+
+// class Horse with Strong, QuickRunner, Tall {}
+
+// class Kid with QuickRunner {}
+
+// void main() {
+//   var player = Player(
+//     team: Team.blue,
+//   );
+
+//   player.runQuick();
 // }
